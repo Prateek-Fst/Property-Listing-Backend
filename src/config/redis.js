2d349a1561e918405b5816b1cb0b963b350d2555
Prateek-Fst <147906999+Prateek-Fst@users.noreply.github.com>
2025-05-29 11:00:20 +0530

    updated redis credentials

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,10 +1,22 @@
-const redis = require('redis');
+const { createClient } = require('redis');
 
-const client = redis.createClient({
-  url: process.env.REDIS_URL,
-});
+async function connectRedis() {
+  const client = createClient({
+    username: 'default',
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+      host:  process.env.REDIS_HOST,
+      port: 10886,
+    },
+  });
 
-client.on('error', (err) => console.log('Redis Client Error', err));
-client.connect();
+  client.on('error', (err) => console.error('Redis Client Error', err));
 
-module.exports = client;
+  await client.connect();
+
+  await client.set('foo', 'bar');
+  const result = await client.get('foo');
+  console.log(result);
+}
+
+connectRedis();
